5f3022fd48ed2efbb067964637ff86ead15b6c00
sudesh2022 <aisudesh777@gmail.com>
2023-08-03 19:13:58 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,11 @@
 import pyjokes
+import json
 
 def main(params):
   joke=pyjokes.get_joke()
-  return {
-    "body": joke
-  } 
+  mydic = { "body": joke}
+  x= json.dumps(mydic)
+  return x
 
 #x = main("")
-#print(x["joke"])
+#print(x)
